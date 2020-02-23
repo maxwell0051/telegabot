23c983a1178aecb60308a62b29f9a7422e49c881
maxwell0051 <maksim.mussin@mail.ru>
2020-02-23 11:09:11 +0500

    rmbot

---
+++ main.py
@@ -0,0 +1,101 @@
+import telebot
+from telebot import types
+import random
+import ctt
+
+strings = ['да', 'нет']
+
+
+bot = telebot.TeleBot('909996276:AAED9HryFPnjNxWQu7ixYwDyBGJY7uB_9_4')
+
+@bot.message_handler(commands=['start'])
+def start(message):
+ bot.send_message(message.chat.id, "Привет!🍪")
+
+ markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+ cmd = types.KeyboardButton("Цитаты")
+ gms = types.KeyboardButton("Игры")
+
+
+
+ markup.add(cmd,gms)
+ bot.send_message(message.chat.id, "Я Бот <b>РАНДОМ</b> ", reply_markup=markup, parse_mode='HTML')
+
+@bot.message_handler(content_types=['text'])
+def text(message):
+    if message.text == 'Цитаты':
+
+
+        markup3 = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        wolf = types.KeyboardButton("Волчьи")
+        normal = types.KeyboardButton("Цитаты Великих Людей")
+        markup3.add(wolf, normal)
+        bot.send_message(message.chat.id, "Выбери тип цитаты", reply_markup=markup3)
+    elif message.text == 'Игры':
+        markup2 = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1 = types.KeyboardButton("🎲Данетка")
+        item2 = types.KeyboardButton("🎲Случайно число")
+        item3 = types.KeyboardButton("🔙Назад")
+
+        markup2.add(item1, item2, item3)
+
+        bot.send_message(message.chat.id, '<b>Выбери игру</b>', parse_mode='HTML', reply_markup=markup2)
+    elif message.text == 'Цитаты':
+            markup3 = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            wolf = types.KeyboardButton("Волчьи")
+            normal = types.KeyboardButton("Цитаты Великих Людей")
+            markup3.add(wolf, normal)
+            bot.send_message(message.chat.id, "Выбери тип цитаты", reply_markup=markup3)
+
+    elif message.text == 'Волчьи':
+            bot.send_message(message.chat.id, random.choice(ctt.wolfs))
+    elif message.text == 'Цитаты Великих Людей':
+            bot.send_message(message.chat.id, random.choice(ctt.norm))
+    else:
+        if message.text == '🎲Данетка':
+            bot.send_message(message.chat.id, 'Задай вопрос, а я отвечу <b>Да</b> или <b>Нет</b>', parse_mode='HTML')
+        elif message.text == '🎲Случайно число':
+            bot.send_message(message.chat.id, str(random.randint(0, 100)))
+        elif message.text == '🔙Назад':
+
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            cmd = types.KeyboardButton("Цитаты")
+            gms = types.KeyboardButton("Игры")
+
+            markup.add(cmd, gms)
+            bot.send_message(message.chat.id, "<i>Возвращаемся</i>", parse_mode='HTML',reply_markup=markup)
+        else:
+            bot.send_message(message.chat.id, "Я думаю что " + random.choice(strings))
+
+
+@bot.message_handler(content_types=['text'])
+def text1(message):
+ if message.text == '🎲Данетка':
+    bot.send_message(message.chat.id, 'Задай вопрос, а я отвечу <b>Да</b> или <b>Нет</b>', parse_mode='HTML')
+ elif message.text == '🎲Случайно число':
+    bot.send_message(message.chat.id, str(random.randint(0,100)))
+ elif message.text == 'Цитаты':
+     markup3 = types.ReplyKeyboardMarkup(resize_keyboard=True)
+     wolf = types.KeyboardButton("Волчьи")
+     normal = types.KeyboardButton("Цитаты Великих Людей")
+     markup3.add(wolf, normal)
+     bot.send_message(message.chat.id, "Выбери тип цитаты", reply_markup=markup3)
+
+     if message.text == 'Волчьи':
+       bot.send_message(message.chat.id, random.choice(ctt.wolfs))
+     elif message.text == 'Цитаты Великих Людей':
+       bot.send_message(message.chat.id, random.choice(ctt.norm))
+
+ else:
+     bot.send_message(message.chat.id, "Я думаю что " + random.choice(strings))
+
+
+
+
+
+
+
+
+
+
+bot.polling()
